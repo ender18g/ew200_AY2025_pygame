dfd88442a5909c11d83d5d83e556c9b948df908a
Allan Elsberry <37273149+ender18g@users.noreply.github.com>
2024-11-12 09:17:51 -0500

    added title

--- game.py
+++ game.py
@@ -3,6 +3,13 @@ import pygame
 from helpers import build_background, kill_ships
 from ship import Ship
 
+# make some colors
+light_blue = pygame.Color('#A2D6F9')
+imp_red = pygame.Color('#F8333C')
+y_blue = pygame.Color('#355070')
+orange = pygame.Color('#FCAB10')
+violet = pygame.Color('#6D597A')
+
 # pygame setup
 pygame.init()
 
@@ -28,6 +35,19 @@ enemy1 = Ship(screen, 400,400, WIDTH, HEIGHT, bullet_group, color='gray')
 ship_group.add(player1) 
 ship_group.add(enemy1)
 
+# make font
+title_font = pygame.font.Font('assets/fonts/Kranky-Regular.ttf',size=80)
+
+# render a font
+title_text = "Life's A Beach"
+title_color = (0,0,255)
+title_surface = title_font.render(title_text,1,imp_red )
+title_rect = title_surface.get_rect()
+title_rect.center = (WIDTH//2, HEIGHT//2)
+bg_alpha = 0
+show_title = 1 # boolean to say if title should be blit
+
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -46,8 +66,18 @@ while running:
     if has_collided:
         player1.kill()
 
+     # make the bg a bit darker
+    background.set_alpha(bg_alpha)
+    title_surface.set_alpha(255-0.4*bg_alpha)
+    bg_alpha +=1
+
+
     # Blit the background to the screen
+    screen.fill((0,0,0))
     screen.blit(background,(0,0))
+
+    # draw text
+    screen.blit(title_surface, title_rect)
 
     # draw the ship
     ship_group.draw(screen)
